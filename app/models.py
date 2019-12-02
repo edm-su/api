eb63cbfe8c045441fa1e68b15378afd9356dab1b
henek <eagluhih@gmail.com>
2019-12-02 15:43:32 +0500

    Для djs и videos добавлено хранение социальных сетей и жанров

--- app/models.py
+++ app/models.py
@@ -42,9 +42,33 @@ class Event(Base):
         return f"Event('{self.name}', '{self.slug}', '{self.image}')"
 
 
+class SocialPage(Base):
+    __tablename__ = 'social_pages'
+    id = Column(Integer, primary_key=True)
+    url = Column(String, nullable=False)
+    network = Column(String, nullable=False)
+    dj_id = Column(Integer, ForeignKey('djs.id'), nullable=False)
+
+    dj = relationship('Dj', back_populates='social_pages')
+
+    def __init__(self, url, network):
+        self.url = url
+        self.network = network
+
+
 djs_videos_table = Table('djs_videos', Base.metadata,
                          Column('dj_id', Integer, ForeignKey('djs.id')),
                          Column('video_id', Integer, ForeignKey('videos.id'))
+                         )
+
+videos_genres_table = Table('videos_genres', Base.metadata,
+                            Column('video_id', Integer, ForeignKey('videos.id')),
+                            Column('genre_id', Integer, ForeignKey('genres.id'))
+                            )
+
+djs_genres_table = Table('djs_genres', Base.metadata,
+                         Column('dj_id', Integer, ForeignKey('djs.id')),
+                         Column('genre_id', Integer, ForeignKey('genres.id'))
                          )
 
 
@@ -55,13 +79,17 @@ class Dj(Base):
     name = Column(String, unique=True, nullable=False)
     slug = Column(String, unique=True, nullable=False)
     image = Column(String, unique=True, nullable=False)
+    country = Column(String)
 
     videos = relationship('Video', back_populates='djs', secondary=djs_videos_table)
+    social_pages = relationship('SocialPage', back_populates='dj')
+    genres = relationship('Genre', back_populates='djs', secondary=djs_genres_table)
 
-    def __init__(self, name, slug, image):
+    def __init__(self, name, slug, image, country=None):
         self.name = name
         self.slug = slug
         self.image = image
+        self.country = country
 
     def __repr__(self):
         return f"Dj('{self.name}', '{self.slug}', '{self.image}')"
@@ -81,6 +109,7 @@ class Video(Base):
     djs = relationship('Dj', back_populates='videos', secondary=djs_videos_table)
     event = relationship('Event', back_populates='videos')
     channel = relationship('Channel', back_populates='videos')
+    genres = relationship('Genre', back_populates='videos', secondary=videos_genres_table)
 
     def __init__(self, title, slug, date, yt_id):
         self.title = title
@@ -90,3 +119,18 @@ class Video(Base):
 
     def __repr__(self):
         return f"Video('{self.title}', '{self.slug}', '{self.date}', '{self.yt_id}')"
+
+
+class Genre(Base):
+    __tablename__ = 'genres'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    slug = Column(String, nullable=False, unique=True)
+
+    videos = relationship('Video', back_populates='genres', secondary=videos_genres_table)
+    djs = relationship('Dj', back_populates='genres', secondary=djs_genres_table)
+
+    def __init__(self, name, slug):
+        self.name = name
+        self.slug = slug
