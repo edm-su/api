1b8b605444dcbd24de963b63a80a099a4f10dfb4
henek <eagluhih@gmail.com>
2020-01-06 16:29:52 +0500

    Добавлены настройки подключения к бд

--- app/database.py
+++ app/database.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.engine.url import URL
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres@db/postgres"
+from app import settings
+
+connect_url = URL(drivername=settings.DB_DRIVER,
+                  username=settings.DB_USERNAME,
+                  password=settings.DB_PASSWORD,
+                  host=settings.DB_HOST,
+                  port=settings.DB_PORT,
+                  database=settings.DB_NAME)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    connect_url
 )
 SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
--- app/settings.py
+++ app/settings.py
@@ -3,3 +3,10 @@ from os import getenv
 EMAIL_FROM = getenv('EMAIL_FROM', 'noreply@edm.su')
 
 SECRET_KEY = getenv('SECRET_KEY', 'bhdasbdashjcxjhzbjhdasjhdasdbasj')
+
+DB_DRIVER = getenv('DB_DRIVER', 'postgresql')
+DB_USERNAME = getenv('DB_USERNAME', 'postgres')
+DB_PASSWORD = getenv('DB_PASSWORD', ' ')
+DB_HOST = getenv('DB_HOST', 'db')
+DB_PORT = getenv('DB_PORT', 5432)
+DB_NAME = getenv('DB_NAME', 'postgres')
