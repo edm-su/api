856d3d80243076dff88b54940acf90318de2eecb
henek <eagluhih@gmail.com>
2020-01-06 18:04:27 +0500

    Добавлены настройки подключения к бд

--- alembic/env.py
+++ alembic/env.py
@@ -6,14 +6,16 @@ sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..'
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy.engine.url import make_url
 
 from alembic import context
-from app.database import Base
+from app import database
 from app import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', str(database.connect_url))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -23,7 +25,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = database.Base.metadata
 
 
 # other values from the config, defined by the needs of env.py,
