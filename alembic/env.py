a373cb28f8272298e987087ea1d07b1fcc591a33
henek <eagluhih@gmail.com>
2020-01-06 18:04:40 +0500

    Добавлены настройки подключения к бд

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,6 @@ sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..'
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from sqlalchemy.engine.url import make_url
 
 from alembic import context
 from app import database
