42803deb0718ab1b792426d03467b4a99cdb3f95
henek <eagluhih@gmail.com>
2020-01-09 20:10:11 +0500

    Увеличено время жизни токена до 7 дней

--- app/utils.py
+++ app/utils.py
@@ -64,7 +64,7 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=30)
+        expire = datetime.utcnow() + timedelta(days=7)
     to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm='HS256')
     return encoded_jwt
