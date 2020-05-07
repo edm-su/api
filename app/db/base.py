bfe9042d9451fdeeab65ca54584e70fe6030741b
henek <eagluhih@gmail.com>
2020-05-07 22:23:57 +0500

    Удалён не используемый код

--- app/db/base.py
+++
@@ -1,5 +0,0 @@
-from app.db.base_class import Base
-from app.models.video import Video
-from app.models.user import User
-from app.models.comment import Comment
-from app.models.channel import Channel
--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.channel import Channel
-from app.models.comment import Comment
-from app.models.user import User
-from app.models.video import Video
-from app.models.post import Post
