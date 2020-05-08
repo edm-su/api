9fcaa227ca0da30fccc441d510b8eeed533f0c7b
henek <eagluhih@gmail.com>
2020-05-08 21:17:55 +0500

    Заголовок x-total-count добавлен в список доступных браузеру

--- app/main.py
+++ app/main.py
@@ -36,5 +36,6 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
+    expose_headers=['x-total-count']
 )
