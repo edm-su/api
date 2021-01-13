7eb79b474af806c6cf93cf2a39eead78d5bb241c
Egor Gluhih <eagluhih@gmail.com>
2021-01-13 19:22:28 +0500

    Исправление авторизации в тестах

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,5 +4,5 @@ from app.auth import create_access_token
 
 
 def create_auth_header(username: str) -> Dict[str, str]:
-    token = create_access_token(data={'sub': username}).decode('utf-8')
+    token = create_access_token(data={'sub': username})
     return {'Authorization': f'Bearer {token}'}
