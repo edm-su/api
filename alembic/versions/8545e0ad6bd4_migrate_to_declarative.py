21355694466103c87a4ce34a1ff720e47f9225ce
Egor Gluhih <eagluhih@gmail.com>
2023-05-19 20:37:57 +0500

    feat: migrate to annotated declarative table
    Fixes: https://github.com/edm-su/api/issues/256

---
+++ alembic/versions/8545e0ad6bd4_migrate_to_declarative.py
@@ -0,0 +1,123 @@
+"""Migrate to declarative
+
+Revision ID: 8545e0ad6bd4
+Revises: 7172bfcfb359
+Create Date: 2023-05-19 20:33:12.730980
+
+"""
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "8545e0ad6bd4"
+down_revision = "7172bfcfb359"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "liked_videos",
+        sa.Column("id", sa.Integer(), nullable=False),
+    )
+    op.alter_column(
+        "liked_videos",
+        "user_id",
+        existing_type=sa.INTEGER(),
+        nullable=False,
+    )
+    op.alter_column(
+        "liked_videos",
+        "video_id",
+        existing_type=sa.INTEGER(),
+        nullable=False,
+    )
+    op.alter_column(
+        "livestreams",
+        "start_time",
+        existing_type=postgresql.TIMESTAMP(timezone=True),
+        nullable=False,
+    )
+    op.drop_index("ix_livestreams_title", table_name="livestreams")
+    op.create_unique_constraint(
+        "unique_livestream",
+        "livestreams",
+        ["start_time", "slug"],
+    )
+    op.alter_column(
+        "posts",
+        "user_id",
+        existing_type=sa.INTEGER(),
+        nullable=False,
+    )
+    op.drop_column("users", "last_login")
+    op.drop_column("users", "last_login_ip")
+    op.alter_column(
+        "videos",
+        "duration",
+        existing_type=sa.INTEGER(),
+        nullable=False,
+        existing_server_default=sa.text("0"),
+    )
+
+
+def downgrade() -> None:
+    op.alter_column(
+        "videos",
+        "duration",
+        existing_type=sa.INTEGER(),
+        nullable=True,
+        existing_server_default=sa.text("0"),
+    )
+    op.add_column(
+        "users",
+        sa.Column(
+            "last_login_ip",
+            sa.VARCHAR(),
+            autoincrement=False,
+            nullable=True,
+        ),
+    )
+    op.add_column(
+        "users",
+        sa.Column(
+            "last_login",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=True,
+        ),
+    )
+    op.alter_column(
+        "posts",
+        "user_id",
+        existing_type=sa.INTEGER(),
+        nullable=True,
+    )
+    op.drop_constraint("unique_livestream", "livestreams", type_="unique")
+    op.create_index(
+        "ix_livestreams_title",
+        "livestreams",
+        ["title"],
+        unique=False,
+    )
+    op.alter_column(
+        "livestreams",
+        "start_time",
+        existing_type=postgresql.TIMESTAMP(timezone=True),
+        nullable=True,
+    )
+    op.alter_column(
+        "liked_videos",
+        "video_id",
+        existing_type=sa.INTEGER(),
+        nullable=True,
+    )
+    op.alter_column(
+        "liked_videos",
+        "user_id",
+        existing_type=sa.INTEGER(),
+        nullable=True,
+    )
+    op.drop_column("liked_videos", "id")
--- app/pkg/postgres.py
+++ app/pkg/postgres.py
@@ -1,11 +1,17 @@
+import datetime
 from collections.abc import AsyncGenerator
+from typing import Any
 
-import sqlalchemy
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.ext.asyncio import (
+    AsyncAttrs,
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
+from sqlalchemy.types import ARRAY, JSON, String
 
 from app.internal.entity.settings import settings
 
@@ -27,153 +33,160 @@ async def get_session() -> AsyncGenerator[AsyncSession, None]:
             yield session
 
 
-metadata = sqlalchemy.MetaData()
-users = sqlalchemy.Table(
-    "users",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column(
-        "username",
-        sqlalchemy.String,
-        nullable=False,
-        unique=True,
-    ),
-    sqlalchemy.Column("email", sqlalchemy.String, unique=True, nullable=False),
-    sqlalchemy.Column("password", sqlalchemy.String, nullable=False),
-    sqlalchemy.Column("is_active", sqlalchemy.Boolean, server_default="f"),
-    sqlalchemy.Column("activation_code", sqlalchemy.String(10)),
-    sqlalchemy.Column("recovery_code", sqlalchemy.String(10)),
-    sqlalchemy.Column("recovery_code_lifetime_end", sqlalchemy.DateTime),
-    sqlalchemy.Column("is_admin", sqlalchemy.Boolean, server_default="f"),
-    sqlalchemy.Column("is_banned", sqlalchemy.Boolean, server_default="f"),
-    sqlalchemy.Column(
-        "created",
-        sqlalchemy.DateTime,
-        nullable=False,
-        server_default=sqlalchemy.sql.func.now(),
-    ),
-    sqlalchemy.Column("last_login", sqlalchemy.DateTime),
-    sqlalchemy.Column("last_login_ip", sqlalchemy.String),
-)
-posts = sqlalchemy.Table(
-    "posts",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("title", sqlalchemy.String, unique=True, nullable=False),
-    sqlalchemy.Column("annotation", sqlalchemy.String),
-    sqlalchemy.Column("text", sqlalchemy.JSON, nullable=False),
-    sqlalchemy.Column("slug", sqlalchemy.String, nullable=False, unique=True),
-    sqlalchemy.Column("published_at", sqlalchemy.DateTime, nullable=False),
-    sqlalchemy.Column("thumbnail", sqlalchemy.String),
-    sqlalchemy.Column(
-        "user_id",
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey("users.id", ondelete="CASCADE"),
-    ),
-)
-videos = sqlalchemy.Table(
-    "videos",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("title", sqlalchemy.String, nullable=False),
-    sqlalchemy.Column("slug", sqlalchemy.String, unique=True, nullable=False),
-    sqlalchemy.Column("date", sqlalchemy.Date),
-    sqlalchemy.Column("yt_id", sqlalchemy.String, unique=True, nullable=False),
-    sqlalchemy.Column("yt_thumbnail", sqlalchemy.String, nullable=False),
-    sqlalchemy.Column("duration", sqlalchemy.Integer, server_default="0"),
-    sqlalchemy.Column("deleted", sqlalchemy.Boolean, server_default="f"),
-)
-liked_videos = sqlalchemy.Table(
-    "liked_videos",
-    metadata,
-    sqlalchemy.Column(
-        "user_id",
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey("users.id"),
-    ),
-    sqlalchemy.Column(
-        "video_id",
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey("videos.id"),
-    ),
-    sqlalchemy.Column(
-        "created_at",
-        sqlalchemy.DateTime,
-        server_default=sqlalchemy.sql.func.now(),
-    ),
-    sqlalchemy.UniqueConstraint(
-        "user_id",
-        "video_id",
-        name="unique_liked_video",
-    ),
-)
-comments = sqlalchemy.Table(
-    "comments",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column(
-        "user_id",
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey("users.id", ondelete="CASCADE"),
-        nullable=False,
-    ),
-    sqlalchemy.Column("text", sqlalchemy.Text, nullable=False),
-    sqlalchemy.Column(
-        "published_at",
-        sqlalchemy.DateTime,
-        nullable=False,
-        server_default=sqlalchemy.sql.func.now(),
-    ),
-    sqlalchemy.Column("deleted", sqlalchemy.Boolean, server_default="f"),
-    sqlalchemy.Column(
-        "video_id",
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey("videos.id", ondelete="CASCADE"),
-        nullable=False,
-    ),
-)
-livestreams = sqlalchemy.Table(
-    "livestreams",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column(
-        "title",
-        sqlalchemy.String(256),
-        nullable=False,
-        index=True,
-    ),
-    sqlalchemy.Column("cancelled", sqlalchemy.Boolean, server_default="f"),
-    sqlalchemy.Column("start_time", sqlalchemy.DateTime(timezone=True)),
-    sqlalchemy.Column("end_time", sqlalchemy.DateTime(timezone=True)),
-    sqlalchemy.Column("image", sqlalchemy.String),
-    sqlalchemy.Column("url", sqlalchemy.String(2083)),
-    sqlalchemy.Column("slug", sqlalchemy.String, nullable=False, index=True),
-    sqlalchemy.Column("genres", sqlalchemy.ARRAY(sqlalchemy.String)),
-)
-users_tokens = sqlalchemy.Table(
-    "users_tokens",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("name", sqlalchemy.String(64), nullable=True),
-    sqlalchemy.Column(
-        "user_id",
-        sqlalchemy.ForeignKey("users.id", ondelete="CASCADE"),
-        nullable=False,
-    ),
-    sqlalchemy.Column(
-        "created_at",
-        sqlalchemy.DateTime(timezone=True),
-        nullable=False,
-        server_default=sqlalchemy.sql.func.now(),
-    ),
-    sqlalchemy.Column(
-        "expired_at",
-        sqlalchemy.DateTime(timezone=True),
-        nullable=True,
-    ),
-    sqlalchemy.Column(
-        "revoked_at",
-        sqlalchemy.DateTime(timezone=True),
-        nullable=True,
-    ),
-)
+class Base(AsyncAttrs, DeclarativeBase):
+    type_annotation_map = {
+        dict[str, Any]: JSON,
+        list[str]: ARRAY(String),
+    }
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
+    is_active: Mapped[bool | None] = mapped_column(server_default="f")
+    activation_code: Mapped[str | None] = mapped_column()
+    recovery_code: Mapped[str | None] = mapped_column()
+    recovery_code_lifetime_end: Mapped[
+        datetime.datetime | None
+    ] = mapped_column()
+    is_admin: Mapped[bool | None] = mapped_column(server_default="f")
+    is_banned: Mapped[bool | None] = mapped_column(server_default="f")
+    created: Mapped[datetime.datetime] = mapped_column(
+        server_default=func.now(),
+    )
+
+    comments: Mapped[list["Comment"]] = relationship(back_populates="user")
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    tokens: Mapped[list["UserToken"]] = relationship(back_populates="user")
+    liked_videos: Mapped[list["LikedVideo"]] = relationship(
+        secondary="LikedVideo",
+        back_populates="users",
+    )
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    annotation: Mapped[str | None] = mapped_column()
+    text: Mapped[dict[str, Any]] = mapped_column()
+    slug: Mapped[str] = mapped_column(unique=True)
+    published_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=func.now(),
+    )
+    thumbnail: Mapped[str | None] = mapped_column()
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "users.id",
+            ondelete="CASCADE",
+        ),
+    )
+
+    user: Mapped["User"] = relationship(
+        back_populates="posts",
+        cascade="all, delete",
+    )
+
+
+class Video(Base):
+    __tablename__ = "videos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column()
+    slug: Mapped[str] = mapped_column(unique=True)
+    date: Mapped[datetime.date | None] = mapped_column()
+    yt_id: Mapped[str] = mapped_column(unique=True)
+    yt_thumbnail: Mapped[str] = mapped_column()
+    duration: Mapped[int] = mapped_column(server_default="0")
+    deleted: Mapped[bool | None] = mapped_column(server_default="f")
+
+    users: Mapped[list["User"]] = relationship(
+        secondary="LikedVideo",
+        back_populates="liked_videos",
+    )
+
+
+class LikedVideo(Base):
+    __tablename__ = "liked_videos"
+    __table_args__ = (
+        UniqueConstraint(
+            "user_id",
+            "video_id",
+            name="unique_liked_video",
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    video_id: Mapped[int] = mapped_column(ForeignKey("videos.id"))
+    created_at: Mapped[datetime.datetime | None] = mapped_column(
+        server_default=func.now(),
+    )
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column()
+    published_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=func.now(),
+    )
+    deleted: Mapped[bool | None] = mapped_column(server_default="f")
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+    )
+    video_id: Mapped[int] = mapped_column(
+        ForeignKey("videos.id", ondelete="CASCADE"),
+    )
+
+    user: Mapped["User"] = relationship(
+        back_populates="comments",
+        cascade="all, delete",
+    )
+
+
+class LiveStream(Base):
+    __tablename__ = "livestreams"
+    __table_args__ = (
+        UniqueConstraint(
+            "start_time",
+            "slug",
+            name="unique_livestream",
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column()
+    slug: Mapped[str] = mapped_column(index=True)
+    cancelled: Mapped[bool | None] = mapped_column(server_default="f")
+    start_time: Mapped[datetime.datetime] = mapped_column()
+    end_time: Mapped[datetime.datetime | None] = mapped_column()
+    image: Mapped[str | None] = mapped_column()
+    url: Mapped[str | None] = mapped_column()
+    genres: Mapped[list[str] | None] = mapped_column()
+
+
+class UserToken(Base):
+    __tablename__ = "users_tokens"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str | None] = mapped_column()
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=func.now(),
+    )
+    expired_at: Mapped[datetime.datetime | None] = mapped_column()
+    revoked_at: Mapped[datetime.datetime | None] = mapped_column()
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+    )
+
+    user: Mapped["User"] = relationship(
+        back_populates="tokens",
+        cascade="all, delete",
+    )
