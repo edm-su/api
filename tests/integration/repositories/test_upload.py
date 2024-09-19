366b19ee0dda855944e102b0c884902169311924
Egor Gluhih <eagluhih@gmail.com>
2024-09-19 19:14:59 +0500

    test: add tests for S3PreSignedUploadRepository.generate method

--- tests/integration/repositories/test_upload.py
+++ tests/integration/repositories/test_upload.py
@@ -3,12 +3,17 @@ from typing import IO
 import pytest
 from typing_extensions import Self
 
-from edm_su_api.internal.usecase.repository.upload import S3UploadRepository
+from edm_su_api.internal.usecase.repository.upload import S3UploadRepository, S3PreSignedUploadRepository
 
 
 @pytest.fixture
 def s3_upload_repository() -> S3UploadRepository:
     return S3UploadRepository()
+
+
+@pytest.fixture
+def s3_pre_signed_upload_repository() -> S3PreSignedUploadRepository:
+    return S3PreSignedUploadRepository()
 
 
 class TestS3Upload:
@@ -21,3 +26,15 @@ class TestS3Upload:
             file=image,
             path="test",
         )
+
+
+class TestS3PreSignedUpload:
+    async def test_generate(
+        self: Self,
+        s3_pre_signed_upload_repository: S3PreSignedUploadRepository,
+    ) -> None:
+        key = "test_key"
+        expires_in = 3600
+        url = await s3_pre_signed_upload_repository.generate(key=key, expires_in=expires_in)
+        assert isinstance(url, str)
+        assert url.startswith("https://")
