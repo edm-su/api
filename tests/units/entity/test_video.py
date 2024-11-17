a27c2a09431ca62fc297b1a46692c03b9ab875cb
Egor Gluhih <eagluhih@gmail.com>
2024-11-17 18:38:16 +0500

    fix: video slug generator (#671)

--- tests/units/entity/test_video.py
+++ tests/units/entity/test_video.py
@@ -24,3 +24,18 @@ class TestNewVideoDto:
         original_slug = new_video.slug
         new_video.expand_slug()
         assert new_video.slug != original_slug
+
+    def test_generate_slug_when_not_provided(self: Self, faker: Faker) -> None:
+        """Test that the slug is generated from the title when not provided."""
+        title = faker.word()
+        video = NewVideoDto(
+            title=title,
+            date=faker.date_between(
+                start_date="-1y",
+                end_date="now",
+            ),
+            yt_id=faker.uuid4(),
+            yt_thumbnail=faker.url(),
+            duration=faker.pyint(),
+        )
+        assert video.slug == title.lower()
